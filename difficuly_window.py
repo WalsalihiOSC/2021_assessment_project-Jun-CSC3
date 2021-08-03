8efabacadf8c3dc61005ef0398ddda47d4cd405d
Jun-CSC3 <st19110@ormiston.school.nz>
2021-08-03 21:08:29 +1200

    minor update 2.0

--- difficuly_window.py
+++ difficuly_window.py
@@ -3,17 +3,22 @@ from user_info import info
 
 BG_colour = "#FAE3C6"
 
-class difficultywindow(Frame):
+class difficultywindow(tk.Frame):
+    # Functions that sets the difficulty of the program
     def click_easy(self):
-        info("Easy")
+        info(name="Easy")
     def click_hard(self):
         info("Hard")
 
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self)
-        main_frame = tk.Frame(background=BG_colour,width=1440,height=900)
+        # Frame within the window
+        # This allows the background to be BG_colour otherwise the background colours
+        # For the buttons will be white
+        main_frame = tk.Frame(background=BG_colour)
         main_frame.pack()
         tk.Label(main_frame,text="Pick your Game Theme",font=("Arial",30),bg=BG_colour).pack()
+        # Easy and Hard buttons 
         easy_btn = tk.Button(main_frame,text="Easy",command=self.click_easy,font=("Arial",25))
         hard_btn = tk.Button(main_frame,text="Hard",command=self.click_hard,font=("Arial",25))
         easy_btn.pack()
@@ -22,5 +27,6 @@ class difficultywindow(Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1440x900")
+    root.configure(bg=BG_colour)
     difficultywindow()
     root.mainloop()
--- user_info.py
+++ user_info.py
@@ -3,9 +3,11 @@ import tkinter as tk
 
 class info():
 
-    def __init__(self,difficulty):
+    def __init__(self,difficulty="this is a test",name=""):
         self.diff = difficulty
         print(self.diff)
+        self.name = name
+        print(self.name)
 
     
 
