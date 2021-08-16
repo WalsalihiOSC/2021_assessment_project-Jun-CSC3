e4e82683e508377795f1bae9eece9398d375d850
Jun-CSC3 <79232120+Jun-CSC3@users.noreply.github.com>
2021-08-17 11:31:36 +1200

    few more features
    added 2 new functions for error and name retrieving corrected the file name

---
+++ difficulty_window.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from user_info import info
+
+BG_colour = "#FAE3C6"
+
+class difficultywindow(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self)
+        # Variable set to represent if the difficulty buttons have been pressed
+        # 0 = no , 1 = yes
+        self.click = 0
+
+        # Frame within the window
+        # This allows the background to be BG_colour otherwise the background colours
+        # For the buttons will be white
+        self.main_frame = tk.Frame(background=BG_colour)
+        self.main_frame.grid(row=5,column=3)
+
+        # Label for name and Entry 
+        name_label = tk.Label(self.main_frame,text="What is your name",font=("Arial",30),bg=BG_colour)
+        name_label.grid(row=2,column=2)
+        self.name_entry = tk.Entry(self.main_frame)
+        self.name_entry.grid(row=3,column=2)
+
+        #Difficulty label
+        name_difficulty = tk.Label(self.main_frame,text="Choose your difficulty",font=("Arial",30),bg=BG_colour)
+        name_difficulty.grid(row=4,column=2)
+        # Easy and Hard buttons 
+        easy_btn = tk.Button(self.main_frame,text="Easy",command=(self.click_easy),font=("Arial",25))
+        hard_btn = tk.Button(self.main_frame,text="Hard",command=(self.click_hard),font=("Arial",25))
+        easy_btn.grid(row=5,column=1)
+        hard_btn.grid(row=5,column=3)
+
+    # Name warning
+    def error(self):
+        self.error_label(self.main_frame,text="broo")
+
+    def entryget(self):
+        # Grabs any input from entry
+        self.entry_text = self.name_entry.get()
+
+        #self.name_entry.delete(0,"end")
+
+    def entrycheck(self):
+        if self.entry_text == "":
+            pass
+
+    # Functions that sets the difficulty of the program
+    def click_easy(self):
+        info(difficulty="Easy")
+        self.click = 1
+        
+    def click_hard(self):
+        info(difficulty="Hard")
+        self.click = 1
+        print(self.click)
+        self.entrycheck()
+
+        
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("1440x900")
+    root.configure(bg=BG_colour)
+    difficultywindow()
+    root.mainloop()
