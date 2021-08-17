e55ef3457d13f97d99a8166e3b6cfbe2fe658a2a
Jun-CSC3 <79232120+Jun-CSC3@users.noreply.github.com>
2021-08-17 15:24:09 +1200

    Update
    finished difficulty window

--- difficulty_window.py
+++ difficulty_window.py
@@ -30,37 +30,46 @@ class difficultywindow(tk.Frame):
         hard_btn = tk.Button(self.main_frame,text="Hard",command=(self.click_hard),font=("Arial",25))
         easy_btn.grid(row=5,column=1)
         hard_btn.grid(row=5,column=3)
-
     # Name warning
     def error(self):
-        self.error_label(self.main_frame,text="broo")
+        self.error_label = tk.Label(self.main_frame,text="broo",bg=BG_colour,fg="#ff0000",font=("Arial",22))
+        self.error_label.grid(row=3,column=3)
 
     def entryget(self):
         # Grabs any input from entry
         self.entry_text = self.name_entry.get()
 
-        #self.name_entry.delete(0,"end")
-
     def entrycheck(self):
+        self.entryget()
         if self.entry_text == "":
-            pass
+            # Checks if the input gathered from the entry is empty
+            # If its an error, calls for the error function
+            self.error()
+        
+        else:
+            self.name_entry.delete(0,"end") 
+            # Forgets/ Removes the error label beside the entry
+            self.error_label.grid_forget()
+            
 
+ 
     # Functions that sets the difficulty of the program
     def click_easy(self):
         info(difficulty="Easy")
         self.click = 1
-        
+        self.entrycheck()
     def click_hard(self):
         info(difficulty="Hard")
         self.click = 1
-        print(self.click)
         self.entrycheck()
 
-        
+    
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1440x900")
     root.configure(bg=BG_colour)
+    root.resizable(False, False)
+
     difficultywindow()
     root.mainloop()
--- selection_window.py
+++ selection_window.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from user_info import info
 from start_window import start_window
-bg_colour = "#FAE3C6"
+
+
+BG_colour = "#FAE3C6"
 
 class selection_window(tk.Frame):
     pass
@@ -10,7 +12,7 @@ class selection_window(tk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry("1440x900")
-    root["bg"] = "#FAE3C6"
+    root.configure(bg=BG_colour)
     root.resizable(False, False)
 
 
